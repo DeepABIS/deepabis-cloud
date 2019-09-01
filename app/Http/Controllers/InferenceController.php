19a58cfd61b2fe93c3b27e9d84ce8d03b1ff8f48
Keanu Buschbacher <keanu.buschbacher@gmail.com>
2019-09-01 22:24:34 +0200

    add docker compose setup, remove python folder, send image over TCP instead of path

--- app/Http/Controllers/InferenceController.php
+++ app/Http/Controllers/InferenceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Request;
 use Storage;
 use Symfony\Component\Process\Process;
@@ -16,26 +17,32 @@ class InferenceController extends Controller
     public function inference(Request $request)
     {
         $file = $request->file('file');
-        Storage::putFile('/predict/', $file);
+        try {
+            $bytes = $file->get();
+        } catch (FileNotFoundException $e) {
+            return response()->json(['error' => 'file not found']);
+        }
 
         if (($socket = socket_create(AF_INET, SOCK_STREAM, 0)) === false) {
             return response('Could not create socket', 500);
         }
 
-        if (($connection = socket_connect($socket, '127.0.0.1', 9000)) === false) {
+        if (($connection = socket_connect($socket, env('INFERENCE_HOST', 'localhost'), env('INFERENCE_PORT', 9042))) === false) {
             return response('Could not connect to server', 500);
         }
 
-        $data = $file->hashName();
-        socket_write($socket, $data, strlen($data));
+        socket_write($socket, pack('J', $file->getSize()), 8);
+
+        $chunks = str_split($bytes, 1024);
+        foreach ($chunks as $chunk) {
+            socket_write($socket, $chunk);
+        }
 
         if (($response = socket_read($socket, 10*1024)) === false) {
             return response('Could not read input', 500);
         }
 
         socket_close($socket);
-
-        Storage::delete('/predict/'.$file->hashName());
 
         $json = json_decode($response);
         $json->uuid = $request->get('uuid');
---
+++ docker/app.dockerfile
@@ -0,0 +1,16 @@
+FROM php:7.1.3-fpm
+RUN apt-get update
+RUN apt-get install -y libmcrypt-dev
+RUN apt-get install -y mysql-client
+RUN apt-get install -y libmagickwand-dev --no-install-recommends
+RUN pecl install imagick
+RUN docker-php-ext-enable imagick
+RUN docker-php-ext-install mcrypt pdo_mysql sockets
+RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+RUN php composer-setup.php
+RUN php -r "unlink('composer-setup.php');"
+RUN mv composer.phar /usr/local/bin/composer
+RUN apt-get install -y git
+RUN apt-get update
+RUN apt-get install -y zlib1g-dev
+RUN docker-php-ext-install zip
